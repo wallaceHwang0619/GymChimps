3c41f0e42fe7cd0fac1d30ca3a4aa2a34eccc2e4
Wallace Hwang <wallacehwang0619@gmail.com>
2022-02-12 13:43:56 -0800

    Survey Shit

---
+++ Survey/Survey/SurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyApp.swift
+//  Survey
+//
+//  Created by Walrus on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
